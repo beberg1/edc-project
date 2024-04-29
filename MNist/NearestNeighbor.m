c8db8b24ebcb9ce4c8714c2c43b3fa3234ba2ed9
Sindre Moldsvor <sindre.moldsvor@gmail.com>
2024-04-29 11:40:22 +0200

    Some changes

--- MNist/NearestNeighbor.m
+++ MNist/NearestNeighbor.m
@@ -1,38 +1,26 @@
-labs = calc_NearestNeighbor(trainv, trainlab, testv, 1000)
+labs = calc_NearestNeighbor(trainv, trainlab, testv)
 
-function labels = calc_NearestNeighbor(train_d, train_l, test_d, chunk_size)
+function labels = calc_NearestNeighbor(train_d, train_l, test_d)
   
-    train_size = size(train_d, 1);
+%     train_size = size(train_d, 1);
     test_size = size(test_d, 1);
     
-    % find amount of chunks
-    n_train_chunks = train_size / chunk_size;
-    %n_test_chunks = test_size / chunk_size;
-
-    % Create arrays for containing distance and corresponding label
+    % Preallocate arrays
     ex_dist = inf*ones(1, test_size);
     ex_index = -1*ones(1, test_size);
 
+    % Calculate distances
     for i = 1:test_size
         test_vec = test_d(i,:);
-
-
-%         for j = 1:n_train_chunks
-%             train_chunk = train_d((j-1)*chunk_size+1:j*chunk_size,:);
-%             D = pdist2(test_vec,train_chunk);
-%             [min_dist, index] = min(D);
-%             
-%             if min_dist < ex_dist(i)
-%                 ex_dist(i) = min_dist;
-%                 ex_index = (1-j)*chunk_size + index;
-%             end
-%         end
-        distances = pdist2(test_vec, train_d);
-        [ex_dist(i), ex_index(i)] = min(distances, );
+        distances = pdist2(test_vec, train_d, "euclidean");
+        [min_dist, index] = min(distances);
+        
+        ex_dist(i) = min_dist;
+        ex_index(i) = index;
         
         fprintf("Done with example %d of %d\n", i, test_size)
     end
 
     % Set correct label
-    labels = train_l(ex_index + 1);
+    labels = train_l(ex_index);
 end
